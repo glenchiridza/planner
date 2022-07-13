ec94a2b1b3c582b8337c8bd3844e5c9eaeaab3f2
glenchiridza <glenchiridz@gmail.com>
2022-07-13 23:51:13 +0200

    app routing

--- planner/client/src/app/app-routing.module.ts
+++ planner/client/src/app/app-routing.module.ts
@@ -1,7 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { TasksComponent } from './tasks/tasks.component';
+import { TodosComponent } from './todos/todos.component';
+import { VideosComponent } from './videos/videos.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:'videos',component:VideosComponent},
+  {path:'addItems',component:TodosComponent},
+  {path:'tasks',component:TasksComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
